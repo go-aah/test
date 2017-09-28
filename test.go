39b43e064333bed3999306f910b872aaf1f1fbae
Jeevanandam M <jeeva@myjeeva.com>
2017-09-28 12:26:01 -0700

    godoc update

--- test.go
+++ test.go
@@ -2,6 +2,8 @@
 // go-aah/test source code and usage is governed by a MIT style
 // license that can be found in the LICENSE file.
 
+// Package test houses all the test compliments to standard Go testing package for
+// aah framework.
 package test
 
 // Version is the `aah` testing library version number
